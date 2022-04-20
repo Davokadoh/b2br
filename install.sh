c6ed3286e784bb728adbfea002a88d5a1baf19af
Joachim Leroux <jleroux@c2r6s5.42lausanne.ch>
2022-04-20 16:42:46 +0200

    Correct comments + stuff

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ apt install sudo openssh-server ufw libpam-pwquality vim wget net-tools
 adduser jleroux sudo
 
 
-/////// SUDO
+#SUDO
 
 mkdir /var/log/sudo
 echo '
@@ -15,20 +15,20 @@ Defaults	logfile="/var/log/sudo/sudolog"
 Defaults	log_input,log_output
 Defaults	iolog_dir="/var/log/sudo"
 Defaults	requiretty
-Defaults	secure_path="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin"' >> /etc/sudoers.d/config
+Defaults	secure_path="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin"' > /etc/sudoers.d/config
 
-/////// SSH
+#SSH
 sed -i -e 's/#Port 22/Port 4242/g' /etc/ssh/sshd_config
 sed -i -e 's/#PermitRootLogin prohibit-password/PermitRootLogin no/g' /etc/ssh/sshd_config
 service ssh restart
-service ssh status
+#service ssh status
 
-/////// UFW
+#UFW
 sudo ufw enable
 sudo ufw allow 4242
 sudo ufw status
 
-/////// PASSWORDS POLICY || USER MANAGEMENT
+#PASSWORDS POLICY || USER MANAGEMENT
 sed -i -e 's/PASS_MAX_DAYS 99999/PASS_MAX_DAYS 30/g' /etc/pam.d/common-password
 sed -i -e 's/PASS_MIN_DAYS 0/PASS_MAX_DAYS 2/g' /etc/pam.d/common-password
 sed -i -e 's/password	requisite pam_pwquality.so retry=3/password	requisite pam_pwquality.so retry=3 minlen=10 ucredit=-1 dcredit=-1 maxrepeat=3 reject_username difok=7 enforce_for_root/g' /etc/pam.d/common-password
@@ -36,7 +36,7 @@ sed -i -e 's/password	requisite pam_pwquality.so retry=3/password	requisite pam_
 addgroup user42
 adduser jleroux user42
 
-/////// CRON
+#CRON
 sudo wget monitoring.sh
 touch tmpfile
 echo '*/10 * * * * sh /usr/local/monitoring.sh' > tmpfile
