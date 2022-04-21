7a0430df9bc4af20de4fc1e6b572842c3bcc289f
Joachim Leroux <jleroux@c2r6s5.42lausanne.ch>
2022-04-21 17:30:17 +0200

    Add lots of sudo to bonus.sh

--- bonus.sh
+++ bonus.sh
@@ -1,43 +1,43 @@
-apt install lighttpd mariadb-server php-cgi php-mysql build-essential pkg-config libssl-dev libpcre2-dev libargon2-0-dev libsodium-dev libc-ares-dev libcurl4-openssl-dev
+sudo apt install lighttpd mariadb-server php-cgi php-mysql build-essential pkg-config libssl-dev libpcre2-dev libargon2-0-dev libsodium-dev libc-ares-dev libcurl4-openssl-dev
 
-ufw allow 80
+sudo ufw allow 80
 echo "Starting mysql_secure_installation"
 echo "Answer no (n) only to the first question"
-mysql_secure_installation
+sudo mysql_secure_installation
 echo "End of mysql_secure_installation"
 
 echo "Creating database"
-mariadb -e "CREATE DATABASE mydb;"
+sudo mariadb -e "CREATE DATABASE mydb;"
 echo "Granting privileges"
-mariadb -e "GRANT ALL ON mydb.* TO dumyuser@localhost WITH GRANT OPTION;"
-mariadb -e "FLUSH PRIVILEGES;"
+sudo mariadb -e "GRANT ALL ON mydb.* TO dumyuser@localhost WITH GRANT OPTION;"
+sudo mariadb -e "FLUSH PRIVILEGES;"
 
 echo "Changing directory"
 cd /var/www/html
 echo "Downloading Wordpress"
-wget http://wordpress.org/latest.tar.gz
+sudo wget http://wordpress.org/latest.tar.gz
 echo "Extracting archive"
-tar -xzvf /var/www/html/latest.tar.gz
+sudo tar -xzvf /var/www/html/latest.tar.gz
 echo "Deleting archive"
-rm /var/www/html/latest.tar.gz
+sudo rm /var/www/html/latest.tar.gz
 echo "Moving files"
-cp -r /var/www/html/wordpress/* /var/www/html
-rm -rf /var/www/html/wordpress
+sudo cp -r /var/www/html/wordpress/* /var/www/html
+sudo rm -rf /var/www/html/wordpress
 echo "Copying config from sample"
-cp /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
+sudo cp /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
 
 echo "Inserting database and user names in config file"
-sed -i -e "s/'database_name_here'/mydb/g" /var/www/html/wp-config.php
-sed -i -e "s/'username_here'/dumyuser/g" /var/www/html/wp-config.php
+sudo sed -i -e "s/'database_name_here'/mydb/g" /var/www/html/wp-config.php
+sudo sed -i -e "s/'username_here'/dumyuser/g" /var/www/html/wp-config.php
 
 echo "Enabling lighttpd mods (?)"
-lighty-enable-mod fastcgi
-lighty-enable-mod fastcgi-php
-service lighttpd force-reload
+sudo lighty-enable-mod fastcgi
+sudo lighty-enable-mod fastcgi-php
+sudo service lighttpd force-reload
 
 cd
-wget https://www.unrealircd.org/downloads/unrealircd-latest.tar.gz
-tar xzvf unrealircd-6.0.3.tar.gz
+sudo wget https://www.unrealircd.org/downloads/unrealircd-latest.tar.gz
+sudo tar xzvf unrealircd-6.0.3.tar.gz
 cd unrealircd-6.0.3
 ./Config
 make
