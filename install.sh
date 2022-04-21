dfa305b77c8b638a1fa176b36650611521d9a79e
Joachim Leroux <jleroux@c2r6s5.42lausanne.ch>
2022-04-21 14:06:33 +0200

    Correct PASS_MAX to PASS_MIN & small stuff

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-apt install sudo openssh-server ufw libpam-pwquality vim wget net-tools
+apt install sudo openssh-server ufw libpam-pwquality vim net-tools
 adduser jleroux sudo
 
 #SUDO
@@ -21,15 +21,16 @@ service ssh restart
 #service ssh status
 
 #UFW
-sudo ufw enable
-sudo ufw allow 4242
-sudo ufw status
+ufw enable
+ufw allow 4242
+#ufw status
 
-#PASSWORDS POLICY || USER MANAGEMENT
+#PASSWORDS POLICY
 sed -i -e 's/PASS_MAX_DAYS 99999/PASS_MAX_DAYS 30/g' /etc/pam.d/common-password
-sed -i -e 's/PASS_MIN_DAYS 0/PASS_MAX_DAYS 2/g' /etc/pam.d/common-password
+sed -i -e 's/PASS_MIN_DAYS 0/PASS_MIN_DAYS 2/g' /etc/pam.d/common-password
 sed -i -e 's/password	requisite pam_pwquality.so retry=3/password	requisite pam_pwquality.so retry=3 minlen=10 ucredit=-1 dcredit=-1 maxrepeat=3 reject_username difok=7 enforce_for_root/g' /etc/pam.d/common-password
 
+#USER MANAGEMENT
 addgroup user42
 adduser jleroux user42
 
@@ -39,5 +40,8 @@ touch tmpfile
 echo '*/10 * * * * sh /usr/local/monitoring.sh' > tmpfile
 crontab -u root tmpfile
 rm tmpfile
+
+#CLEANUP
+rm install.sh
 
 reboot now
