2e4cb3e93e36c6440eb4ba7420c92afec78c413d
Joachim Leroux <jleroux@c2r6s5.42lausanne.ch>
2022-04-22 12:12:21 +0200

    add -f flags to tar command

--- bonus.sh
+++ bonus.sh
@@ -37,7 +37,7 @@ sudo service lighttpd force-reload
 
 cd /home/jleroux
 sudo wget https://www.unrealircd.org/downloads/unrealircd-latest.tar.gz
-sudo tar -x unrealircd-latest.tar.gz
+sudo tar -xf unrealircd-latest.tar.gz
 cd unrealircd-6.0.3
 chmod u+rw unrealircd-6.0.3
 ./Config
